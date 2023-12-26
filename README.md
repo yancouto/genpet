7bc97ad88441aab715b584e7d9f7e754b359af62
Eric Brandwein <brandweineric@gmail.com>
2023-12-26 01:34:29 -0300

    Add example

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 Generate petgraph graphs with geng.
 
+## Examples
+    
+```rust
+use genpet::generate_graphs;
+use genpet::GengOption;
+
+let graphs = generate_graphs(3, &[]);
+
+for graph in graphs {
+    println!("{}", graph.node_count());
+}
+
+let graphs = generate_graphs(4, &[GengOption::Connected, GengOption::Chordal]);
+for graph in graphs {
+    println!("{}", graph.node_count());
+}
+```
+
 ## Testing locally
 
 - Install Rust and Cargo
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
 //! # genpet
 //! 
 //! Generate petgraph graphs using the geng program from nauty.
-
+//!
+//! ## Example
+//! ```
+//! use genpet::generate_graphs;
+//! use genpet::GengOption;
+//! 
+//! let graphs = generate_graphs(3, &[]);
+//! 
+//! for graph in graphs {
+//!     println!("{}", graph.node_count());
+//! }
+//! 
+//! let graphs = generate_graphs(4, &[GengOption::Connected, GengOption::Chordal]);
+//! for graph in graphs {
+//!     println!("{}", graph.node_count());
+//! }
+//! ```
 mod generate;
 pub use crate::generate::generate_graphs;
 pub use crate::generate::geng::GengOption;
